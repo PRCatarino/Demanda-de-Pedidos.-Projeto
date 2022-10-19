53266ea09e80388330c5f98374cf8296536cc8c0
PRCatarino <paulo061104@gmail.com>
2022-10-19 19:05:19 -0300

    Pagina incial funcionando

---
+++ assets/data.js
@@ -0,0 +1,54 @@
+const listProduct  = [
+    {
+        codigo:"1001",
+        product:"Super SMACH COMBO Programado – Hambúrguer + Fritas",
+        price: 55.00,
+    },
+    {
+        codigo:"1002",
+        product:"SMACH VariavelBurguer – Hambúrguer com bacon",
+        price:45.00,
+    },
+    {
+        codigo:"1003",
+        product:"SMACH BUG EM PROD – Hambúrguer meio torto",
+        price: 25.00,
+    },
+    {
+        codigo:"1004",
+        product:"Combo Econômico SMACH Char 1 – Pão com Carne",
+        price: 15.00,
+    },
+    {
+        codigo:"2001",
+        product:"Refrigerante 350 ml",
+        price:8.00,
+    },
+    {
+        codigo:"2002",
+        product:"Água 500 ml",
+        price:5.00,
+    },
+    {
+        codigo:"2003",
+        product:"Suco 350 ml",
+        price: 7.00,
+    },
+    {
+        codigo:"3001",
+        product:"Sorvete 300 ml",
+        price: 15.00,
+    },
+    {
+        codigo:"1004",
+        product:"Combo Econômico SMACH Char 1 – Pão com Carne",
+        price:15.00,
+    },
+    {
+        codigo:"3002",
+        product:"Sobremesa doce SMACH ARRAY",
+        price:50.00,
+    },
+];
+
+export default listProduct;
--- index.js
+++ index.js
@@ -1,86 +1,59 @@
-let listProduct  = [
-    {
-        codigo:"1001",
-        product:"Super SMACH COMBO Programado – Hambúrguer + Fritas",
-        price: 55.00,
-    },
-    {
-        codigo:"1002",
-        product:"SMACH VariavelBurguer – Hambúrguer com bacon",
-        price:45.00,
-    },
-    {
-        codigo:"1003",
-        product:"SMACH BUG EM PROD – Hambúrguer meio torto",
-        price: 25.00,
-    },
-    {
-        codigo:"1004",
-        product:"Combo Econômico SMACH Char 1 – Pão com Carne",
-        price: 15.00,
-    },
-    {
-        codigo:"2001",
-        product:"Refrigerante 350 ml",
-        price:8.00,
-    },
-    {
-        codigo:"2002",
-        product:"Água 500 ml",
-        price:5.00,
-    },
-    {
-        codigo:"2003",
-        product:"Suco 350 ml",
-        price: 7.00,
-    },
-    {
-        codigo:"3001",
-        product:"Sorvete 300 ml",
-        price: 15.00,
-    },
-    {
-        codigo:"1004",
-        product:"Combo Econômico SMACH Char 1 – Pão com Carne",
-        price:15.00,
-    },
-    {
-        codigo:"3002",
-        product:"Sobremesa doce SMACH ARRAY",
-        price:50.00,
-    },
-];
-
-
+import listProduct from '/assets/data.js'
+let produto = null
 
 function addEvent(e){
-    document.querySelector('.searchBtn').addEventListener('click', buscaProduto);
+    document.querySelector('.searchBtn').addEventListener('click', preencherProdutoBusca);
+    document.querySelector('.quantity').addEventListener('change', changeQuantity);
+    document.querySelector('.btnAdd').addEventListener('click',  addProduct);
 }
-function buscaProduto(){
-    let price = document.querySelector('.price');
+function searchProduct(codigoProduto){
+    let produtoEncontrado = listProduct.find(product =>{
+        
+        return product.codigo == codigoProduto
+    });
+    
+   return produtoEncontrado;
+}
+    
+function preencherProdutoBusca(){
     let productName = document.querySelector('.productName');
     let codigoProduto = document.querySelector('.searchCod').value;
-    let produtoEncontrado = listProduct.find(product =>{
-        
-        return product.codigo == codigoProduto
-    });
+    let produtoEncontrado = searchProduct(codigoProduto)
     productName.setAttribute("value", produtoEncontrado.product);
-    price.setAttribute('value', produtoEncontrado.price);
+    
+    produto = produtoEncontrado
+    calc(produtoEncontrado.price)
 }
 
-function calcula(){
-    let quantity = document.querySelector('.quantity');
-    let codigoProduto = document.querySelector('.searchCod').value;
-    let produtoEncontrado = listProduct.find(product =>{
-        
-        return product.codigo == codigoProduto
-    });
-    let elementTotal = document.querySelector('.price')
-   
-    let calc = Number(produtoEncontrado.price) * Number(quantity.value);
+function calc(price){
 
-    elementTotal.setAttribute("value", calc);
+    const quantity = document.querySelector('.quantity')
+    let elementPrice = document.querySelector('.price')
+   
+    let result = Number(price) * Number(quantity.value);
+    
+    elementPrice.setAttribute('value', `R$${result}`);
+}
+function changeQuantity(){
+    calc(produto.price)
+}
+
+function addProduct(){
+    const code = document.querySelector('.searchCod').value;
+    const quantity = document.querySelector('.quantity').value;
+    const product = document.querySelector('.productName').value;
+    const price = document.querySelector('.price').value;
+    const table = document.querySelector('tbody');
+
+    table.innerHTML += `
+    <tr>
+        <td class="codeTable">${code}</td>
+        <td class="productTable">${product}</td>
+        <td class="quantityTable">${quantity}</td>
+        <td class="valueProductTable">${price}</td>
+    </tr>
+    
+    `
     
 }
-
 addEvent();
