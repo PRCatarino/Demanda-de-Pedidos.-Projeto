4499577726b09835354446b54d8d5c028d6ed508
PRCatarino <paulo061104@gmail.com>
2022-10-21 12:36:32 -0300

    site funcionando

--- index.js
+++ index.js
@@ -4,18 +4,38 @@ const button = document.querySelector('.btnAdd');
 const alert = document.getElementById('boxAlert');
 const btnSave = document.getElementById('saveBtn');
 const arrItems = [];
+let sumTotal = 0
 let produto = null;
 let contador = 0;
 let productName = '';
 let codigoProduto = '';
 
-
 function addEvent(){
-    document.querySelector('.searchBtn').addEventListener('click', preencherProdutoBusca);
+    document.querySelector('.searchBtn').addEventListener('click', checkFilled);
     document.querySelector('.quantity').addEventListener('change', changeQuantity);
     document.querySelector('.btnAdd').addEventListener('click',  addProduct);
+    document.querySelector('.saveBtn').addEventListener('click', saveProduct );
+    document.querySelector('.newOrder').addEventListener('click',newRequest);
+    document.querySelector('.saveBtn').addEventListener('click',saveProductTable);
 }
 
+function checkFilled(){
+    let inputSearch = document.getElementById('searchCode');
+    codigoProduto = document.querySelector('.searchCod').value;
+    document.querySelector('.btnAdd').removeAttribute('disabled')
+    alert.classList.remove('alertOn')
+    alert.classList.add('alertOff');
+
+    if(codigoProduto != ''){
+        inputSearch.classList.remove('alertActive')
+        button.style.backgroundColor = 'var(--cor-secundaria-orange-46)'
+        preencherProdutoBusca()
+    }else{
+        inputSearch.classList.add('alertActive')
+        alert.classList.remove('alertOff');
+        alert.classList.add('alertOn')
+    }
+}
 
 function searchProduct(codigoProduto){
     let produtoEncontrado = listProduct.find(product =>{
@@ -26,28 +46,14 @@ function searchProduct(codigoProduto){
 
     
 function preencherProdutoBusca(){
-    let inputSearch = document.getElementById('searchCode');
+    
     productName = document.querySelector('.productName');
-    codigoProduto = document.querySelector('.searchCod').value;
     
-    document.querySelector('.btnAdd').removeAttribute('disabled')
-    alert.classList.remove('alertOn')
-    alert.classList.add('alertOff');
-    
-    if(codigoProduto != ''){
-        let produtoEncontrado = searchProduct(codigoProduto);
-        inputSearch.classList.remove('alertActive')
-        button.style.backgroundColor = 'var(--cor-secundaria-orange-46)'
+    let produtoEncontrado = searchProduct(codigoProduto);
+    productName.setAttribute("value", produtoEncontrado.product);
+    produto = produtoEncontrado
+    calc(produtoEncontrado.price)
 
-        productName.setAttribute("value", produtoEncontrado.product);
-        produto = produtoEncontrado
-        calc(produtoEncontrado.price)
-}   else{
-        inputSearch.classList.add('alertActive')
-
-        alert.classList.remove('alertOff');
-        alert.classList.add('alertOn')
-    }
 }
  
 
@@ -88,12 +94,41 @@ function addProduct(){
     `  
     contador++
     
-    let sumTotal = arrItems.reduce((total,atual)=>{
+    sumTotal = arrItems.reduce((total,atual)=>{
         return total + atual.total;
     },0);
 
     document.querySelector('.valueTotal').innerHTML = `R$ ${sumTotal}, 00`
- 
+}
+function saveProduct(){
+    document.getElementById('page1').classList.add('ocultSection')
+    document.getElementById('page2').classList.remove('ocultSection')
+    
+}
+function newRequest(){
+    document.getElementById('page1').classList.remove('ocultSection')
+    document.getElementById('page2').classList.add('ocultSection')
+}
+
+function saveProductTable(){
+    const valueRadio = document.querySelector('input[name="tipoConsumo"]:checked').value;
+    
+    let product = ''
+    let table = document.querySelector('.tBody');
+    if(valueRadio != ''){
+        for(let i = 0; i < arrItems.length; i++){
+            product += `${arrItems[i].quantity} - ${arrItems[i].product}</br>`;
+     }
+        table.innerHTML +=`
+            <tr>
+                <td>${1}</td>
+                <td>${product}</td>
+                <td>${valueRadio}</td>
+                <td>R$${sumTotal},00</td>
+                <td>Pendente</td>
+            </tr>
+        `
+    }
 }
 
 addEvent()
